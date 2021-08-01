687f73b88e1b4207c2c2de1c1c9b9b96766d7a4e
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-08-01 17:29:38 -0500

    added basic layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    setMessages([...messages, input]);
+    setInput('');
+  }
+
+  console.log(input);
+  console.log(messages);
+
   return (
     <div className="App">
       <h1>Hello Clever Programmer</h1>
+
+      <form>
+        <input value={input} onChange={e => setInput(e.target.value)}/>
+        <button type="submit" onClick={sendMessage}>Send Message</button>
+      </form>
+
+      {
+        messages.map((message, index) => (
+          <p key={index}>{message}</p>
+        ))
+      }
     </div>
   );
 }
